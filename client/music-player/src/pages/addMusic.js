9788babaa31f52730fde86624f2738f90ab4826d
neeraj2403 <jayarajneeraj@gmail.com>
2022-06-24 14:17:17 +0530

    add music

--- client/music-player/src/pages/addMusic.js
+++ client/music-player/src/pages/addMusic.js
@@ -4,12 +4,44 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import AddArtist from './addArtist'
 
 import Select from 'react-select'
-
+// import {useEffect} from 'react'
+import React, { useState,useEffect } from "react";
+import Axios from 'axios';
 
 
 // import './header.css'
 function AddMusic(){
+
+  const [artistName,setArtistName] = useState([]);
+
+  const [name,setName] = useState("");
+  const [dor,setDor] = useState("");
+  const [artist,setArtist] = useState("");
+
+  const getArtist = () =>{
+    Axios.get('http://localhost:3001/getArtist').then((response) =>{
+        setArtistName(response.data)
+        console.log(artistName)
+
+    });
+
+    }
+    const addSong = () =>{
+      Axios.post("http://localhost:3001/addSong",{
+        name: name,
+        dor: dor,
+        artist: artist,
+      }).then(() =>{
+        console.log("success")
     
+      }
+      );
+     }
+
+  // useEffect(() => {
+  //    getArtist()
+    
+  //   },[]);
 
   return (
     <>
@@ -17,19 +49,41 @@ function AddMusic(){
     <Form>
   <Form.Group className="mb-3" controlId="formBasicEmail">
     <Form.Label>Song name</Form.Label>
-    <Form.Control type="text"  />
+    <Form.Control type="text" 
+    onChange = {(event) => {setName(event.target.value)}}
+    />
 
   </Form.Group>
 
   <Form.Group className="mb-3" controlId="formBasicPassword">
     <Form.Label>Date released</Form.Label>
-    <Form.Control type="text"  />
+    <Form.Control type="text" 
+    onChange = {(event) => {setDor(event.target.value)}}
+    />
   </Form.Group>
-  <div>
-  <Select></Select>
-  <span> <AddArtist /></span>
-  </div>
-  <Button variant="primary" type="submit">
+
+  <Form.Select aria-label="Default select example" 
+  onChange = {(event) => {setArtist(event.target.value)}}>
+  {artistName.map((val=> 
+  <option value={val.Name}>{val.Name}</option>
+  ))}
+
+</Form.Select>
+   
+    
+        
+   
+
+
+
+    <span> <AddArtist /></span>
+
+
+ 
+  <Button onClick = {getArtist} variant="primary" >
+  get artist
+</Button>
+  <Button variant="primary" type="submit" onClick = {addSong}>
     Submit
   </Button>
 </Form></div>
--- server/index.js
+++ server/index.js
@@ -19,6 +19,8 @@ const db = new sqlite3.Database("./spotify.db", sqlite3.OPEN_READWRITE, (err) =>
     else return console.log("connected")
 });
 
+
+
 app.post("/addArtist", (req,res) =>{
     const name = req.body.name;
     const dob = req.body.dob;
@@ -28,6 +30,53 @@ app.post("/addArtist", (req,res) =>{
     db.run(
         "INSERT INTO Artist (Name,DOB,Bio) VALUES (?,?,?)",
         [name,dob,bio],
+        (err,result) =>{
+            if(err) {
+                console.log(err)
+            }else{
+                res.send("values inserted")
+            }
+        }
+    );
+
+
+});
+
+
+// db.getConnection(function (err,connection) {
+//     if (err) {
+//       console.log(err);
+//     }
+//   });
+
+
+
+app.get('/getArtist',(req,res) => {
+    db.all("SELECT id,Name FROM Artist",(err,result) =>{
+        if(err){
+            console.log(err)
+        }else{
+            res.send(result)
+            console.log("sucess")
+            console.log(result)
+        }
+    }
+    )
+})
+
+
+
+app.post("/addSong", (req,res) =>{
+    const name = req.body.name;
+    const dor = req.body.dor;
+    const artist = req.body.artist;
+    const rating = 0;
+    const users = 0
+    // console.log(dob)
+    
+    db.run(
+        "INSERT INTO Songs (Name,Date_of_release,Artist,Average_rating,no_users) VALUES (?,?,?,?,?)",
+        [name,dor,artist,rating,users],
         (err,result) =>{
             if(err) {
                 console.log(err)
